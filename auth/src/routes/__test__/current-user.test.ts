5319d8ebf480477a546ac6290869c2a84bb6a614
AntonPiniazOverdose <anton.p@overdose.digital>
2021-02-01 23:18:51 +0200

    Added tests to project

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responce with details about the current user', async () => {
+  const cookie = await global.signin();
+
+  const responce = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(responce.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responce with null if not authenticated', async () => {
+  const response = await request(app).get('/api/users/currentuser').send();
+  expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
